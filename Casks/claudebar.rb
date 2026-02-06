ec41a7b3a4fd2ed85454220179b3782a09e33555
Kemal Aslıyüksek <kemal@kemalasliyuksek.com>
2026-02-06 16:26:37 +0300

    Add ClaudeBar cask formula v1.0.0

---
+++ Casks/claudebar.rb
@@ -0,0 +1,18 @@
+cask "claudebar" do
+  version "1.0.0"
+  sha256 "51afd3d40c0460ef781e0a942e4954dd735902bbb25643f3a1b84c048f4eebc2"
+  url "https://github.com/kemalasliyuksek/ClaudeBar/releases/download/v#{version}/ClaudeBar-v#{version}-macos.zip"
+  name "ClaudeBar"
+  desc "Native macOS menu bar app for monitoring Claude usage limits"
+  homepage "https://github.com/kemalasliyuksek/ClaudeBar"
+  depends_on macos: ">= :sonoma"
+  app "ClaudeBar.app"
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-cr", "#{appdir}/ClaudeBar.app"],
+                   sudo: false
+  end
+  zap trash: [
+    "~/Library/Preferences/com.kemalasliyuksek.ClaudeBar.plist",
+  ]
+end
